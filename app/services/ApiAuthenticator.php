a23489f05f932c9ce1b30077c8c67b133ce26db4
Vlasta Neubauer <paranoiq@centrum.cz>
2013-08-22 13:17:37 +0200

    added ApiAuthenticator

---
+++ app/services/ApiAuthenticator.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Clevis\RestApi;
+
+use DateTime;
+use Clevis\Users\Authenticator;
+use Nette\Security\AuthenticationException;
+use Nette\Object;
+use Nette\Utils\Strings;
+
+
+/**
+ * Zajišťuje přihlášení uživatele klíčem API
+ *
+ * - pokud je nastaven Authenticator, pokusí se uživatele přihlásit jménem a heslem a vytvoří mu API klíč
+ * - pokud je $lifeTime (resp. User::$apiKeyExpirationDate) NULL, má klíč platnost navždy
+ */
+class ApiAuthenticator extends Object implements IApiAuthenticator
+{
+
+	/** @var string */
+	private $lifeTime = '+30 days';
+
+	/** @var UsersRepository */
+	private $users;
+
+	/** @var Authenticator */
+	private $authenticator;
+
+
+	public function __construct(UsersRepository $users, Authenticator $authenticator = NULL)
+	{
+		$this->users = $users;
+		$this->authenticator = $authenticator;
+	}
+
+	/**
+	 * Nastavuje dobu platnosti nového kliče API. např. "+30 days"
+	 *
+	 * @param string
+	 */
+	public function setApiKeyLifetime($lifeTime)
+	{
+		$this->lifeTime = $lifeTime;
+	}
+
+	/**
+	 * @param string|int
+	 * @param array|NULL
+	 * @return IApiUser|NULL
+	 */
+	public function authenticate($apiKey, $requestData = NULL)
+	{
+		/** @var User $user */
+		$user = $this->users->getByApiKey($apiKey);
+		if ($user && !$user->apiKeyExpirationDate || $user->apiKeyExpirationDate > new DateTime)
+		{
+			return $user;
+		}
+
+		if (!$requestData || !$this->authenticator)
+		{
+			return NULL;
+		}
+
+		try
+		{
+			// zkusí uživatele přihlásit
+			$user = $this->authenticator->authenticate($requestData);
+
+			$user->apiKey = $this->createApiKey($user);
+			if ($this->lifeTime)
+			{
+				$user->apiKeyExpirationDate = new DateTime($this->lifeTime);
+			}
+
+			$this->users->persitAndflush($user);
+
+			return $user;
+		}
+		catch (AuthenticationException $e)
+		{
+			return NULL;
+		}
+	}
+
+	/**
+	 * Vytváří API klíč uživatele
+	 *
+	 * @param User
+	 * @return string
+	 */
+	private function createApiKey(User $user)
+	{
+		return Strings::random(40);
+	}
+
+}
